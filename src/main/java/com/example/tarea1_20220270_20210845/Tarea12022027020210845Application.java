d94936b8672713958daf68d3490e88eb90d66f90
Christian Flores <167637198+Cjfs2005@users.noreply.github.com>
2025-04-09 20:46:29 -0500

    Proyecto creado

---
+++ src/main/java/com/example/tarea1_20220270_20210845/Tarea12022027020210845Application.java
@@ -0,0 +1,13 @@
+package com.example.tarea1_20220270_20210845;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea12022027020210845Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tarea12022027020210845Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tarea1_20220270_20210845/Tarea12022027020210845ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tarea1_20220270_20210845;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea12022027020210845ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
